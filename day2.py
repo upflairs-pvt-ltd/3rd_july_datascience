88a2db035eec8c4738c26522ff84e3b1fc92564f
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-07-04 11:35:11 +0530

    4thjuly

---
+++ day2.py
@@ -0,0 +1,111 @@
+# _________ Variables ______________ 
+# int var = 10;  comments
+
+# var = 20.25
+# print(var)
+# print(type(var))
+
+
+# int var = 10;
+# int  ===> data type 
+# var  ===> identifier 
+# =    ===> assignment operator
+
+
+
+#1. A-Z , a-z , 0 -9 , _ only these characters 
+#2. variable name can not start with number 
+#3. 
+
+# age  = 25 
+# Age = 26
+# AGE = 30 
+# print(AGE)  # 30 
+# print(Age)  # 26 
+# print(age)  # 25
+
+# age = 25 
+# age = 30
+# print(age)
+
+
+# Reserved Keywords 
+
+# var1 = 20 
+# var2 = 30 
+
+# var@ = 25
+# var =  
+# _ = 25 
+# print(_)
+
+
+# int , float  
+# datatype    
+# <<<<<<<<<<< string >>>>>>>>>>>>>
+
+# st = "upflairs pvt ltd"
+# st = 'upflairs pvt ltd'
+# st = """upflairs pvt ltd"""
+
+# print(st)
+# print(type(st))
+
+
+# st = 'upflairs'
+# [starting : stoping : jump]
+# print(st[4])
+# print(st[4:7])
+# print(st[-1])
+# print(st[2:])
+# print(st[:4])
+# print(st[::2])
+# print(st[::-1])
+
+# st = "upflairs pvt ltd jaipur rajasthan"
+
+
+# jaipur 
+# print(st[17::23])
+# print(st[17:24])
+# print(st[16:23])
+# print(len(st))  # total no. of char 
+
+
+# st = "upflairs pvt ltd jaipur"
+# st2 = "UPFLAIRS PVT LTD JAIPUR"
+# print(st.count("ajasthan"))
+# print(st)
+# print(st.upper())
+# print(st2.lower())  # 
+
+# st2.title()
+# st2.capitalize()
+
+
+# st = "Upflairs pvt ltd jaipur"
+# print(st)
+# print(st.replace('Upflairs','flipkart'))
+# print(st.replace('Upflairs',''))
+
+st = 'upflairs'
+
+# print(st.find('u'))
+# st.endswith('s')
+# st.startswith()
+# st.split()
+# st.strip()
+# st.center()
+
+
+
+
+
+
+
+
+
+
+
+
+
